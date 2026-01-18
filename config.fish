3a832ef4fb1303a91b14634e29cadd50a66385cc
Ivy Duggan <ivy@csd.dev>
2026-01-18 14:05:57 -0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -53,24 +53,28 @@ function vew --description 'Cat/bat a file or list a directory in detail | spell
   end
 end
 
-# TODO: handle when the input is a command via a script, not an installed package
 # https://itsfoss.gitlab.io/post/how-to-find-a-package-version-in-linux
 function version --description 'Check if a package/command is installed, then display its version'
   set item "$argv"
+
   if command -v "$item" &>/dev/null
     echo -n "[$(command -v "$item")]: "
-    if command -v "dpkg" &>/dev/null
-      dpkg -s "$item" | grep Version | awk '{ print $2 }'
-    else if command -v "pacman" &>/dev/null
-      pacman -Qi "$item" | grep Version | awk '{ print $3 }'
-    else if command -v "dnf" &>/dev/null
-      dnf info "$item" | grep Version
-    else if command -v "rpm" &>/dev/null
-      rpm -qi "$item" | grep Version
-    else if command -v "zypper" &>/dev/null
-      zypper info "$item" | grep Version
-    else if command -v "apk" &>/dev/null
-      apk info "$item" | grep Version
+    if not which "$item" &>/dev/null
+      echo "script/alias, no version found!"
+    else
+      if command -v "dpkg" &>/dev/null
+        dpkg -s "$item" | grep Version | awk '{ print $2 }'
+      else if command -v "pacman" &>/dev/null
+        pacman -Qi "$item" | grep Version | awk '{ print $3 }'
+      else if command -v "dnf" &>/dev/null
+        dnf info "$item" | grep Version
+      else if command -v "rpm" &>/dev/null
+        rpm -qi "$item" | grep Version
+      else if command -v "zypper" &>/dev/null
+        zypper info "$item" | grep Version
+      else if command -v "apk" &>/dev/null
+        apk info "$item" | grep Version
+      end
     end
     return 0
   end
